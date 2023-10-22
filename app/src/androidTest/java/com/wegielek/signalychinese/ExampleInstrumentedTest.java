e2bd00771cfa0255200fa4dc901145ccc0577b59
nestrockx <72703958+nestrockx@users.noreply.github.com>
2023-10-22 11:15:35 +0200

    Added Junit5

--- app/src/androidTest/java/com/wegielek/signalychinese/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/wegielek/signalychinese/ExampleInstrumentedTest.java
@@ -1,26 +1,20 @@
 package com.wegielek.signalychinese;
 
-import android.content.Context;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import androidx.test.platform.app.InstrumentationRegistry;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
+import de.mannodermaus.junit5.ActivityScenarioExtension;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+
+@ExtendWith(ActivityScenarioExtension.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.wegielek.signalychinese", appContext.getPackageName());
+
     }
 }
